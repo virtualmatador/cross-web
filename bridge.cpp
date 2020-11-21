67cd984e6085e69eaaeef6acf90c2e5ca3562909
Ali Asadpoor <virtualmatador@yahoo.com>
2020-11-21 01:58:03 -0500

    Load image view

--- bridge.cpp
+++ bridge.cpp
@@ -1,19 +1,80 @@
+#include <condition_variable>
+#include <mutex>
 #include <sstream>
+#include <iostream>
+#include <list>
+#include <vector>
 
 #include "emmintrin.h"
+#include "emscripten/threading.h"
 
 #include "../core/src/interface.h"
 
 #include "../core/src/bridge.h"
 
+std::condition_variable work_condition_;
+std::mutex work_mutex_;
+bool exit_{ false };
+bool need_restart_{ false };
+struct Message
+{
+    std::int32_t sender;
+    const char* id;
+    const char* command;
+    const char* info;
+};
+std::list<Message> messages_;
+std::list<std::string> functions_;
+std::vector<uint32_t> pixels_;
+std::mutex pixels_lock_;
+
+extern "C"
+{
+    void PostJsMessage(const std::int32_t sender, const char* id, const char* command, const char* info)
+    {
+        bridge::PostThreadMessage(sender, id, command, info);
+    }
+
+    const char* PickFunction()
+    {
+        return functions_.front().c_str();
+    }
+
+    void PopFunction()
+    {
+        functions_.pop_front();
+    }
+
+    void* CreatePixels(const std::int32_t image_width, const std::int32_t image_height)
+    {
+        std::lock_guard<std::mutex> auto_lock{ pixels_lock_ };
+        pixels_.resize(image_width * image_height);
+        return pixels_.data();
+    }
+
+    void LockPixels()
+    {
+        pixels_lock_.lock();
+    }
+
+    void UnlockPixels()
+    {
+        pixels_lock_.unlock();
+    }
+}
+
 void bridge::NeedRestart()
 {
-    interface::Restart();
+    {
+        std::lock_guard<std::mutex> auto_lock{ work_mutex_ };
+        need_restart_ = true;
+    }
+    work_condition_.notify_one();
 }
 
 void bridge::LoadWebView(const std::int32_t sender, const std::int32_t view_info, const char *html, const char* waves)
 {
-    EM_ASM(
+    MAIN_THREAD_ASYNC_EM_ASM(
     {
         LoadWebView($0, $1, $2, $3);
     }, sender, view_info, html, waves);
@@ -21,7 +82,7 @@ void bridge::LoadWebView(const std::int32_t sender, const std::int32_t view_info
 
 void bridge::LoadImageView(const std::int32_t sender, const std::int32_t view_info, const std::int32_t image_width, const char* waves)
 {
-    EM_ASM(
+    MAIN_THREAD_ASYNC_EM_ASM(
     {
         LoadImageView($0, $1, $2, $3);
     }, sender, view_info, image_width, waves);
@@ -29,23 +90,34 @@ void bridge::LoadImageView(const std::int32_t sender, const std::int32_t view_in
 
 std::uint32_t* bridge::GetPixels()
 {
-    return nullptr;
+    pixels_lock_.lock();
+    return pixels_.data();
 }
 
 void bridge::ReleasePixels(std::uint32_t* const pixels)
 {
+    pixels_lock_.unlock();
 }
 
 void bridge::RefreshImageView()
 {
+    MAIN_THREAD_ASYNC_EM_ASM(
+    {
+        var px = Module.ccall('LockPixels', null, null, null);
+        RefreshImageView();
+        Module.ccall('UnlockPixels', null, null, null);
+    });
 }
 
 void bridge::CallFunction(const char* function)
 {
-    std::ostringstream js;
-    js << "document.getElementById('web_view').contentWindow.eval('" <<
-        function << "');";
-    emscripten_run_script(js.str().c_str());
+    functions_.push_back(function);
+    MAIN_THREAD_ASYNC_EM_ASM(
+    {
+        document.getElementById('web_view').contentWindow.eval(
+            Module.ccall("PickFunction", 'string', null, null));
+        Module.ccall("PopFunction", null, null, null)
+    });
 }
 
 std::string bridge::GetAsset(const char* key)
@@ -64,6 +136,11 @@ void bridge::SetPreference(const char* key, const char* value)
 
 void bridge::PostThreadMessage(const std::int32_t sender, const char* id, const char* command, const char* info)
 {
+    {
+        std::lock_guard<std::mutex> auto_lock{ work_mutex_ };
+        messages_.push_back({sender, id, command, info});
+    }
+    work_condition_.notify_one();
 }
 
 void bridge::AddParam(const char* key, const char* value)
@@ -84,4 +161,41 @@ void bridge::Exit()
     {
         location = "about:blank";
     });
+}
+
+int main()
+{
+    interface::Begin();
+    interface::Create();
+    interface::Start();
+    for(;;)
+    {
+        std::unique_lock<std::mutex> work_locker{ work_mutex_ };
+        work_condition_.wait(work_locker, [&]()
+        {
+            return exit_ || need_restart_ || !messages_.empty();
+        });
+        if (exit_)
+        {
+            break;
+        }
+        if (need_restart_)
+        {
+            need_restart_ = false;
+            work_locker.unlock();
+            interface::Restart();
+        }
+        while(!messages_.empty())
+        {
+            Message msg = messages_.front();
+            messages_.pop_front();
+            work_locker.unlock();
+            interface::HandleAsync(msg.sender, msg.id, msg.command, msg.info);
+            work_locker.lock();
+        }
+    }
+    interface::Stop();
+    interface::Destroy();
+    interface::End();
+    return 0;
 }
--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 var view_id = 0;
+var pixels;
+var mapped_buffer;
 
 function CallHandler(id, command, info)
 {
-    Module.ccall('HandleAsync', null, ['number', 'string', 'string', 'string'], [view_id, id, command, info]);
-}
-
-Module['onRuntimeInitialized'] = function()
-{
-    Module._Begin();
-    Module._Create();
-    Module._Start();
+    Module.ccall('PostJsMessage', null, ['number', 'string', 'string', 'string'], [view_id, id, command, info]);
 }
 
 function LoadWebView(sender, view_info, html, waves)
 {
     view_id = sender;
+    document.getElementById('image_view').style.display= 'none';
     document.getElementById('web_view').onload = function()
     {
+        document.getElementById('web_view').contentWindow.CallHandler = CallHandler;
+        document.getElementById('web_view').style.display = 'block';
         CallHandler('body', 'ready', '');
     };
-    document.getElementById('web_view').src = 'assets/html/' + UTF8ToString(html) + '.htm';
+    document.getElementById('web_view').data = 'assets/html/' + UTF8ToString(html) + '.htm';
+}
+
+function LoadImageView(sender, view_info, image_width, waves)
+{
+    view_id = sender;
+    document.getElementById('web_view').style.display = 'none';
+    document.getElementById('web_view').data = '';
+    document.getElementById('image_view').style.display = 'block';
+    var image_height = Math.floor(image_width *
+        document.getElementById('image_view').height /
+        document.getElementById('image_view').width);
+    document.getElementById('image_view').width = image_width;
+    document.getElementById('image_view').height = image_height;
+    var ctx = document.getElementById('image_view').getContext("2d");
+    pixels = ctx.createImageData(image_width, image_height);
+    var pixelsData = Module.ccall('CreatePixels', null, ['number', 'number'], [image_width, image_height]);    
+    mapped_buffer = new Uint8ClampedArray(Module.HEAPU8.buffer, pixelsData, image_width * image_height * 4);
+    CallHandler('body', 'ready', image_width / 10 + ' ' + image_width + ' ' + image_height + ' ' + 33619971);
+}
+
+function RefreshImageView()
+{
+    var ctx = document.getElementById('image_view').getContext("2d");
+    pixels.data.set(mapped_buffer);
+    ctx.putImageData(pixels, 0, 0);
 }
